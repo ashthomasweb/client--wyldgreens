b30b2a847f6621f7689f7772a4f7b45dc984f992
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-07-04 23:03:19 -0400

    Add Plan selection buttons
    Duplicates style from Mix buttons with sliding pane. Adds checkmark and
    'Added' indication. Only one plan selection is now selectable at one
    time.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 // Main JavaScript file for "LumberJack"
 
 
+// MOCK PAGE MENU
+
 // function mockPage(input) {
 //     let host = window.location.hostname;
 //     console.log(host);
@@ -10,18 +12,21 @@
 //     } else {
 //         window.location.href = window.location.protocol + '//' + host + '/' + input;
 //     }
-
-
 // }
+
+
+
+// LIGHTBOX
 
 let lightboxBool = false;
 let lightboxPane = document.querySelector('.lightbox');
+
 function lightbox() {
-    if ( lightboxBool === true ) {
+    if (lightboxBool === true) {
         lightboxPane.style.opacity = "0";
         lightboxPane.pointerEvents = 'none';
 
-    } else if ( lightboxBool === false ) {
+    } else if (lightboxBool === false) {
         lightboxPane.pointerEvents = 'auto';
         lightboxPane.style.opacity = "1";
     }
@@ -42,14 +47,140 @@ function lightboxOff() {
 
 
 
+// ADD CHECK TO FORM
+
+function addToOrder(input) {
+    document.querySelector(`[name="${input}"]`).checked = true;
+}
+
+function clearPlansForm() {
+    document.querySelector('[name=weekly]').checked = false;
+    document.querySelector('[name=bulk]').checked = false;
+    document.querySelector('[name=one_time]').checked = false;
+}
+
+// PLAN SELECTION BUTTONS
+
+let weeklyPane = document.querySelector('#weekly-pane');
+let weeklyOrderBtn = document.querySelector('#weekly-order-btn');
+let weeklyAdded = document.querySelector('#weekly-added');
+
+let oneTimePane = document.querySelector('#one_time-pane');
+let oneTimeOrderBtn = document.querySelector('#one_time-order-btn');
+let oneTimeAdded = document.querySelector('#one_time-added');
+
+let bulkPane = document.querySelector('#bulk-pane');
+let bulkOrderBtn = document.querySelector('#bulk-order-btn');
+let bulkAdded = document.querySelector('#bulk-added');
+
+function weeklyOpen() {
+    weeklyPane.classList.add('plan-btn-pane-open');
+    weeklyOrderBtn.classList.add('center-btn-clicked');
+    weeklyAdded.classList.add('plan-added-clicked');
+}
+
+function weeklyClose() {
+    weeklyDelBool = false;
+
+    weeklyPane.classList.remove('plan-btn-pane-open');
+    weeklyOrderBtn.classList.remove('center-btn-clicked');
+    weeklyAdded.classList.remove('plan-added-clicked');
+}
+
+function oneTimeOpen() {
+    oneTimePane.classList.add('plan-btn-pane-open');
+    oneTimeOrderBtn.classList.add('sides-btn-clicked');
+    oneTimeAdded.classList.add('plan-added-clicked');
+}
+
+function oneTimeClose() {
+    oneTimeDelBool = false;
+
+    oneTimePane.classList.remove('plan-btn-pane-open');
+    oneTimeOrderBtn.classList.remove('sides-btn-clicked');
+    oneTimeAdded.classList.remove('plan-added-clicked');
+}
+
+function bulkOpen() {
+    bulkPane.classList.add('plan-btn-pane-open');
+    bulkOrderBtn.classList.add('sides-btn-clicked');
+    bulkAdded.classList.add('plan-added-clicked');
+}
+
+function bulkClose() {
+    bulkDelBool = false;
+
+    bulkPane.classList.remove('plan-btn-pane-open');
+    bulkOrderBtn.classList.remove('sides-btn-clicked');
+    bulkAdded.classList.remove('plan-added-clicked');
+}
+
+
+let weeklyDelBool = false;
+function weeklyDel() {
+    if (weeklyDelBool === false) {
+        weeklyDelBool = !weeklyDelBool;
+        weeklyOpen();
+        oneTimeClose();
+        bulkClose();
+        document.querySelector('.weekly-btn-check').style.display = "block";
+        document.querySelector('.one_time-btn-check').style.display = "none";
+        document.querySelector('.bulk-btn-check').style.display = "none";
+        clearPlansForm();
+        addToOrder('weekly');
+    } else if (weeklyDelBool === true) {
+        weeklyDelBool = !weeklyDelBool;
+        weeklyClose();
+        clearPlansForm();
+    }
+}
+
+let bulkDelBool = false;
+function bulkDel() {
+    if (bulkDelBool === false) {
+        bulkDelBool = !bulkDelBool;
+        weeklyClose();
+        bulkOpen();
+        oneTimeClose();
+        document.querySelector('.weekly-btn-check').style.display = "none";
+        document.querySelector('.one_time-btn-check').style.display = "none";
+        document.querySelector('.bulk-btn-check').style.display = "block";
+        clearPlansForm();
+        addToOrder('bulk');
+    } else if (bulkDelBool === true) {
+        bulkDelBool = !bulkDelBool;
+        clearPlansForm();
+        bulkClose();
+    }
+}
+
+let oneTimeDelBool = false;
+
+function oneTimeDel() {
+    if (oneTimeDelBool === false) {
+        oneTimeDelBool = !oneTimeDelBool;
+        weeklyClose();
+        bulkClose();
+        oneTimeOpen();
+        document.querySelector('.weekly-btn-check').style.display = "none";
+        document.querySelector('.one_time-btn-check').style.display = "block";
+        document.querySelector('.bulk-btn-check').style.display = "none";
+        clearPlansForm();
+        addToOrder('one_time');
+    } else if (oneTimeDelBool === true) {
+        oneTimeDelBool = !oneTimeDelBool;
+        oneTimeClose();
+        clearPlansForm();
+    }
+}
+
+// CUSTOM MIX FORM
+
 let orderBtn = document.querySelector(".custom-order-button");
 let hiddenBtn = document.querySelector(".custom-order-hidden");
 let ingredChecks = document.querySelectorAll('.custom-check');
 let orderBox = document.querySelector('.ingred-box');
 
-function addToOrder(input) {
-    document.querySelector(`[name="${input}"]`).checked = true;
-}
 
 function customOrderOn() {
     orderBtn.innerText = "Pick up to 4";
@@ -123,31 +254,31 @@ function ingredCheck() {
 let customQuanBool = false;
 
 function promptQuantity() {
-    
+
     let customChecks = document.querySelectorAll('.custom-check');
     let prevText = document.querySelector("#message-body").value;
     let itemList = [];
-    
-    if (customQuanBool === true ) {
+
+    if (customQuanBool === true) {
         customQuanBool = false;
-        
+
         for (i = 0; i < document.querySelectorAll('.custom-check').length; i++) {
             customChecks[i].checked === true && itemList.push(customChecks[i].name);
         }
 
         let boxVar = "box";
-        if ( Number(document.querySelector(".custom-quantity input").value) > 1 ) {
+        if (Number(document.querySelector(".custom-quantity input").value) > 1) {
             boxVar = "boxes";
         }
 
         document.querySelector(".wyld-form-mix input[name=custom-num]").value = Number(document.querySelector(".wyld-form-mix input[name=custom-num]").value) + Number(document.querySelector(".custom-quantity input").value);
         document.querySelector("#message-body").value = `I would like ${document.querySelector(".custom-quantity input").value} custom order ${boxVar} with the following:${itemList}.\n\n${prevText}`;
-        
+
         addToOrder('mix_custom');
         hiddenBtn.style.right = "-1px";
 
         customOrderOff();
-    } else if ( customQuanBool === false ) {
+    } else if (customQuanBool === false) {
         customQuanBool = true;
         hiddenBtn.innerText = "Add to Order Form";
         document.querySelector('.custom-btn-slider').classList.add('custom-btn-slider-anim');
@@ -155,7 +286,10 @@ function promptQuantity() {
     }
 }
 
+// MIX SELECTION BUTTONS
+
 let healthyBool = false;
+
 function healthyMix() {
     if (healthyBool === false) {
         healthyBool = !healthyBool;
@@ -165,7 +299,7 @@ function healthyMix() {
         document.querySelector("#healthy-pane").classList.add("mix-btn-pane-open");
         setTimeout(() => {
             document.querySelector("#healthy-pane.mix-btn-pane").style.transition = "box-shadow .1s ease-out";
-        }, 400 );
+        }, 400);
 
     } else if (healthyBool === true) {
         healthyBool = !healthyBool;
@@ -174,7 +308,7 @@ function healthyMix() {
         document.querySelector("#healthy-pane").classList.remove("mix-btn-pane-open");
         setTimeout(() => {
             document.querySelector("#healthy-pane.mix-btn-pane").style.transition = "box-shadow .25s ease-out .1s";
-        }, 200 );
+        }, 200);
 
         document.querySelector("#healthy-order-btn").innerText = "View Order";
         document.querySelector(".wyld-form-mix input[name=healthy-num]").value = document.querySelector("#healthy-quantity input").value;
@@ -182,7 +316,22 @@ function healthyMix() {
     }
 }
 
-let saladBool = false; 
+function healthyInputCheck() {
+    let input = Number(document.querySelector("#healthy-quantity input").value);
+    let button = document.querySelector("#healthy-order-btn");
+    if (input === 0) {
+        button.style.pointerEvents = "none";
+        button.style.color = "#666";
+    } else if (input > 0) {
+        button.style.pointerEvents = "auto";
+        button.style.color = "#222";
+    }
+}
+
+
+
+let saladBool = false;
+
 function saladMix() {
     if (saladBool === false) {
         saladBool = !saladBool;
@@ -192,7 +341,7 @@ function saladMix() {
         document.querySelector("#salad-pane").classList.add("mix-btn-pane-open");
         setTimeout(() => {
             document.querySelector("#salad-pane.mix-btn-pane").style.transition = "box-shadow .1s ease-out";
-        }, 400 );
+        }, 400);
     } else if (saladBool === true) {
         saladBool = !saladBool;
         document.querySelector("#salad-order-btn").classList.remove('mix-button-clicked');
@@ -201,7 +350,7 @@ function saladMix() {
 
         setTimeout(() => {
             document.querySelector("#salad-pane.mix-btn-pane").style.transition = "box-shadow .25s ease-out .1s";
-        }, 200 );
+        }, 200);
         document.querySelector("#salad-order-btn").innerText = "View Order";
         document.querySelector(".wyld-form-mix input[name=salad-num]").value = document.querySelector("#salad-quantity input").value;
         addToOrder('mix_salad');
@@ -209,6 +358,7 @@ function saladMix() {
 }
 
 let spicyBool = false;
+
 function spicyMix() {
     if (spicyBool === false) {
         spicyBool = !spicyBool;
@@ -218,7 +368,7 @@ function spicyMix() {
         document.querySelector("#spicy-pane").classList.add("mix-btn-pane-open");
         setTimeout(() => {
             document.querySelector("#spicy-pane.mix-btn-pane").style.transition = "box-shadow .1s ease-out";
-        }, 400 );
+        }, 400);
     } else if (spicyBool === true) {
         spicyBool = !spicyBool;
         document.querySelector("#spicy-order-btn").classList.remove('mix-button-clicked');
@@ -227,7 +377,7 @@ function spicyMix() {
 
         setTimeout(() => {
             document.querySelector("#spicy-pane.mix-btn-pane").style.transition = "box-shadow .25s ease-out .1s";
-        }, 200 );
+        }, 200);
         document.querySelector("#spicy-order-btn").innerText = "View Order";
         document.querySelector(".wyld-form-mix input[name=spicy-num]").value = document.querySelector("#spicy-quantity input").value;
         addToOrder('mix_spicy');
@@ -235,6 +385,7 @@ function spicyMix() {
 }
 
 let farmersBool = false;
+
 function farmersMix() {
     if (farmersBool === false) {
         farmersBool = !farmersBool;
@@ -244,7 +395,7 @@ function farmersMix() {
         document.querySelector("#farmers-pane").classList.add("mix-btn-pane-open");
         setTimeout(() => {
             document.querySelector("#farmers-pane.mix-btn-pane").style.transition = "box-shadow .1s ease-out";
-        }, 400 );
+        }, 400);
     } else if (farmersBool === true) {
         farmersBool = !farmersBool;
         document.querySelector("#farmers-order-btn").classList.remove('mix-button-clicked');
@@ -253,50 +404,12 @@ function farmersMix() {
 
         setTimeout(() => {
             document.querySelector("#farmers-pane.mix-btn-pane").style.transition = "box-shadow .25s ease-out .1s";
-        }, 200 );
+        }, 200);
         document.querySelector("#farmers-order-btn").innerText = "View Order";
         document.querySelector(".wyld-form-mix input[name=farmers-num]").value = document.querySelector("#farmers-quantity input").value;
         addToOrder('mix_farmers');
     }
 }
-
-function healthyInputCheck() {
-    let input = Number(document.querySelector("#healthy-quantity input").value);
-    let button = document.querySelector("#healthy-order-btn");
-    if ( input === 0 ) {
-        button.style.pointerEvents = "none";
-        button.style.color = "#666";
-    } else if ( input > 0 ) {
-        button.style.pointerEvents = "auto";
-        button.style.color = "#222";
-    }
-}
-
-
-// let barsPos = 0;
-// let barsRot = 0;
-// let bar2Vis = 1;
-// let menuPos = 20;
-
-// let animA = setInterval(barMid, 8);
-
-// // middle bar disappear
-// function barMid() {
-//     if (bar2Vis < 0.1) {
-//         // normally clearInterval would be executed when value reaches 0, but due to bit arithmetic 
-//         // 0.1 - 0.1 is displayed in extended notation. 
-//         clearInterval(animA);
-//         // reset opacity to typical value for consistency.
-//         bar2Vis = 0;
-//         bar2.style.opacity = bar2Vis;
-//     } else {
-//         bar2Vis = bar2Vis - 0.1;
-//         bar2.style.opacity = bar2Vis;
-//     }
-// }
-
-
-
 
 
 // || Contact form check for field input then change button color 
