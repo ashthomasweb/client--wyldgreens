98f8bb08ea012cfe0e25b521327542a667b12fe0
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-07-28 01:33:49 -0400

    Add plane animation to /mock5
    Banner, prop, and plane now independent images, animated to give
    impression of changing altitude and waving banner. Not implemented on
    current mock page, found on /mock5.

--- public/js/main.js
+++ public/js/main.js
@@ -74,3 +74,37 @@ let plane = document.querySelector('#wyld-plane');
 function fromRight() {
     plane.classList.add('plane-go-right');
 }
+
+let planeWrap = document.querySelector('.plane-wrap');
+let bannerWrap = document.querySelector('.banner-wrap');
+
+function planeUp() {
+    planeWrap.classList.add('plane-wrap-up');
+    bannerWrap.classList.add('banner-wrap-up');
+    setTimeout( () => {
+        plane.style.top = '450px';
+    }, 300 )
+    setTimeout( () => {
+        planeLevel();
+    }, 8300)
+
+}
+
+function planeDown() {
+    planeWrap.classList.add('plane-wrap-down');
+    bannerWrap.classList.add('banner-wrap-down');
+    setTimeout( () => {
+        plane.style.top = '550px';
+    }, 300)
+    setTimeout( () => {
+        planeLevel();
+    }, 8300)
+}
+
+function planeLevel() {
+    planeWrap.classList.remove('plane-wrap-up');
+    bannerWrap.classList.remove('banner-wrap-up');
+    planeWrap.classList.remove('plane-wrap-down');
+    bannerWrap.classList.remove('banner-wrap-down');
+
+}
