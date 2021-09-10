afdae7169628d2e0517221d979c2ccb8b3a2739a
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-10 15:04:44 -0400

    Update app.js
    Remove localhost and heroku port listener. Probable cause of multiple
    processes spawning on host.

--- app.js
+++ app.js
@@ -68,9 +68,9 @@ app.post('/', function (req, res) {
 // || Listener 
 
 let port = process.env.PORT;
-if (port == null || port == "") { 
-    port = 3000; 
-}
+// if (port == null || port == "") { 
+//     port = 3000; 
+// }
 app.listen(port, () => console.log(`Server started at port ${port}.`));
 
 
