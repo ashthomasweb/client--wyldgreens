2149baafddf53b77364f632c6b5c965313d513e6
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-09 14:47:01 -0500

    Adds comments for deliverable
    Basic maintenance notes put in app.js.

--- app.js
+++ app.js
@@ -1,148 +1,161 @@
-// Node.js/Express Server "app.js" for "Wyldgreens" 
+// Node.js/Express Server "app.js" for "Wyldgreens"
 
-// Dependencies 
-require('dotenv').config();
+// Dev note: Issues with current architecture led to NodeMailer not functioning correctly in any attempted module setups.
+// Leave nodemailer functions and templates in post route if hosted with A2 hosting. Small server file allows for this 
+// to not be a performance issue at current scale. Possible that on a newly built server even at A2 module exporting could work.
+// App duplication occurs when a non-SSL connection is attempted, potentially due to the current setup where wyldgreens is a subdomain 
+// of the parent. 
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const favicon = require('express-favicon');
+// Ideally site should be re-architected on a host that allows external smtp, authorization performed with OAuth.
+// Current registrar GoDaddy creates problems with most hosting companies that are node friendly and affordable.
+// Potential solution that requires DNS migration and full refactor of nodemailer authorization is heroku, DNSimple, 
+// and Google OAuth with a Google for Business email. This service architecture will not work with any current 
+// configuration - it's all or nothing. 
+
+// GoDaddy usage of both email provider and domain registrar limits the technology and infrastructure choices due to proprietary systems.
+// Usage of Godaddy email requres exteral smtp, however usage of GoDaddy registrar means limited technology choices for external hosting,
+// or expensive internal unmanaged hosting, without direct node support.
+// No change in host recommended at this time.
+
+// Do so with caution and time for email refactorization, dns migration, ssl certification, dns propagation, and functional testing in the new environment.
+
+// In cPanel, on any host, remember to run 'NPM Install' in the node setup window to automatically install all dependencies AFTER uploading site files, 
+// and to restart the application any time the app.js file is altered.
+
+
+
+// Dependencies
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const favicon = require("express-favicon");
 const nodemailer = require("nodemailer");
 const secure = require("ssl-express-www");
 
 const app = express();
-app.use(secure)
+app.use(secure);
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', function (req, res) {
-    res.render('home', {
-        pageTitle: "Wyldgreens",
-        responseBool: false,
-    });
+app.get("/", function (req, res) {
+  res.render("home", {
+    pageTitle: "Wyldgreens",
+    responseBool: false,
+  });
 });
 
-app.post('/', function (req, res) {
+app.post("/", function (req, res) {
 
-    // Issues with current architecture led to nodemailer not functioning correctly in any attempted module setups. 
-    // Leave nodemailer functions and templates in post route for A2 hosting. Small server file allows for this to not be a performance issue at current scale.
-    // Ideally should be re-architected on a host that allows external smtp, authorization performed with OAuth.
-    // Current registrar GoDaddy creates problems with hosting companies that are node friendly.
-    // Potential solution that requires DNS migration and full refactor of nodemailer authorization is heroku, DNSimple, and Google OAuth with a Google for Business email.
-    // Tied usage of email provider and domain registrar limits the technology and infrastructure choices due to proprietary systems.
-    // Usage of Godaddy email requres exteral smtp, however usage of GoDaddy registrar means limited technology choices for external hosting, 
-    // or expensive internal unmanaged hosting, without direct node support. 
-    // No change in host recommended at this time. 
-    // 
-    // Do so with caution and time for email refactorization, dns migration, ssl certification, dns propagation, and functional testing in the new environment.
+  let ifError = false;
 
-    let ifError = false;
+  // Data recieved from the contact form
+  let {
+    user_name,
+    user_email,
+    user_city,
+    user_phone,
+    message,
+    one_time,
+    weekly,
+    bulk,
+    farmers_num,
+    order1_num,
+    order1_ing,
+    order2_num,
+    order2_ing,
+    order3_num,
+    order3_ing,
+    healthy_num,
+    salad_num,
+    spicy_num,
+    total_price,
+  } = req.body;
 
-    // Data recieved from the contact form
-    let {
-        user_name,
-        user_email,
-        user_city,
-        user_phone,
-        message,
-        one_time,
-        weekly,
-        bulk,
-        farmers_num,
-        order1_num,
-        order1_ing,
-        order2_num,
-        order2_ing,
-        order3_num,
-        order3_ing,
-        healthy_num,
-        salad_num,
-        spicy_num,
-        total_price
-    } = req.body;
+  // Nodemailer functions and templates for "Wyldgreens"
 
-    // Nodemailer functions and templates for "Wyldgreens" 
+  // Mailer transport object
+  var transporter = nodemailer.createTransport({
+    host: "mi3-ts3.a2hosting.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
 
-    // Mailer transport object 
-    var transporter = nodemailer.createTransport({
-        host: 'mi3-ts3.a2hosting.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-        }
-    });
+  function scheduleChoice() {
+    if (one_time !== undefined) {
+      return "One Time Delivery";
+    } else if (weekly !== undefined) {
+      return "Weekly Delivery";
+    } else if (bulk !== undefined) {
+      return "Bulk Delivery";
+    }
+  }
 
-    function scheduleChoice() {
-        if (one_time !== undefined) {
-            return 'One Time Delivery'
-        } else if (weekly !== undefined) {
-            return 'Weekly Delivery'
-        } else if (bulk !== undefined) {
-            return 'Bulk Delivery'
-        }
+  function mixChoice() {
+    let farmers = "";
+    let healthy = "";
+    let salad = "";
+    let spicy = "";
+
+    if (Number(farmers_num) > 0) {
+      farmers = `Farmer's Mix - Quantity: ${farmers_num}<br>`;
     }
 
-    function mixChoice() {
-        let farmers = '';
-        let healthy = '';
-        let salad = '';
-        let spicy = '';
-
-        if (Number(farmers_num) > 0) {
-            farmers = `Farmer's Mix - Quantity: ${farmers_num}<br>`;
-        }
-
-        if (Number(healthy_num) > 0) {
-            healthy = `Healthy Mix - Quantity: ${healthy_num}<br>`;
-        }
-
-        if (Number(salad_num) > 0) {
-            salad = `Salad Mix - Quantity: ${salad_num}<br>`;
-        }
-
-        if (Number(spicy_num) > 0) {
-            spicy = `Spicy Mix - Quantity: ${spicy_num}<br>`;
-        }
-
-        return farmers + healthy + salad + spicy;
+    if (Number(healthy_num) > 0) {
+      healthy = `Healthy Mix - Quantity: ${healthy_num}<br>`;
     }
 
-    function customChoice() {
-        let order1 = '';
-        let order2 = '';
-        let order3 = '';
-
-        if (Number(order1_num) > 0) {
-            order1 = `Custom Mix - Quantity: ${order1_num}<br><em>${order1_ing}</em><br><br>`;
-        }
-
-        if (Number(order2_num) > 0) {
-            order2 = `Custom Mix - Quantity: ${order2_num}<br><em>${order2_ing}</em><br><br>`;
-        }
-
-        if (Number(order3_num) > 0) {
-            order3 = `Custom Mix - Quantity: ${order3_num}<br><em>${order3_ing}</em>`;
-        }
-
-        return order1 + order2 + order3;
+    if (Number(salad_num) > 0) {
+      salad = `Salad Mix - Quantity: ${salad_num}<br>`;
     }
 
-    function subjectLineDate() {
-        return new Date().toString().slice(0, 16);
+    if (Number(spicy_num) > 0) {
+      spicy = `Spicy Mix - Quantity: ${spicy_num}<br>`;
     }
 
-    // Templates
-    function inquiryTemplate() {
+    return farmers + healthy + salad + spicy;
+  }
 
-        let inqTemplate = `
+  function customChoice() {
+    let order1 = "";
+    let order2 = "";
+    let order3 = "";
+
+    if (Number(order1_num) > 0) {
+      order1 = `Custom Mix - Quantity: ${order1_num}<br><em>${order1_ing}</em><br><br>`;
+    }
+
+    if (Number(order2_num) > 0) {
+      order2 = `Custom Mix - Quantity: ${order2_num}<br><em>${order2_ing}</em><br><br>`;
+    }
+
+    if (Number(order3_num) > 0) {
+      order3 = `Custom Mix - Quantity: ${order3_num}<br><em>${order3_ing}</em>`;
+    }
+
+    return order1 + order2 + order3;
+  }
+
+  function subjectLineDate() {
+    return new Date().toString().slice(0, 16);
+  }
+
+  // Templates
+  function inquiryTemplate() {
+    let inqTemplate = `
     
         <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
             <h2>Hi Max, looks like an order is coming in.</h2>
@@ -168,13 +181,12 @@ app.post('/', function (req, res) {
     
         `;
 
-        let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-        return output;
-    };
+    let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+    return output;
+  }
 
-    function confirmTemplate() {
-
-        let confTemplate = `
+  function confirmTemplate() {
+    let confTemplate = `
     
         <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
             <h2>Hi ${user_name}, thanks for checking out Wyldgreens.</h2>
@@ -203,57 +215,58 @@ app.post('/', function (req, res) {
 
         `;
 
-        let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-        return output;
-    };
+    let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+    return output;
+  }
 
-    // Nodemailer email objects
-    function mailNewInquiry(user_name, user_email, message) {
-        return `{"from": "order@wyldgreens.com",
+  // Nodemailer email objects
+  function mailNewInquiry(user_name, user_email, message) {
+    return `{"from": "order@wyldgreens.com",
     "to": "alek@alekhess.com",
     "subject": "New Wyld order! From: ${user_name} on ${subjectLineDate()}",
     "html": "${inquiryTemplate()}"}`;
-    };
+  }
 
-    function mailConfirmation(user_name, user_email, message) {
-        return `{"from": "order@wyldgreens.com",
+  function mailConfirmation(user_name, user_email, message) {
+    return `{"from": "order@wyldgreens.com",
     "to": "${user_email}",
     "subject": "Wyld order succesfully placed on ${subjectLineDate()}",
     "html": "${confirmTemplate()}"}`;
-    };
+  }
 
-    // Object parsing
-    let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
-    let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
+  // Object parsing
+  let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
+  let finalConfirm = JSON.parse(
+    mailConfirmation(user_name, user_email, message)
+  );
 
-    // Transporter objects
-    var userInquiry = transporter.sendMail(inquiry);
-    var userConfirm = transporter.sendMail(finalConfirm);
+  // Transporter objects
+  var userInquiry = transporter.sendMail(inquiry);
+  var userConfirm = transporter.sendMail(finalConfirm);
 
-    // Upon completion, sends response to page indicating success or failure.
-    Promise.all([userInquiry, userConfirm])
-        .then(([resultInq, resultConf]) => {
-            console.log("Emails sent", resultInq, resultConf);
-        })
-        .catch((err) => {
-            console.log(err);
-            ifError = true;
-        })
-        .finally(() => {
-            res.render('home', {
-                pageTitle: "Wyldgreens",
-                responseBool: true,
-                isError: ifError,
-            });
-            transporter.close();
-        });
-
+  // Upon completion, sends response to page indicating success or failure.
+  Promise.all([userInquiry, userConfirm])
+    .then(([resultInq, resultConf]) => {
+      console.log("Emails sent", resultInq, resultConf);
+    })
+    .catch((err) => {
+      console.log(err);
+      ifError = true;
+    })
+    .finally(() => {
+      res.render("home", {
+        pageTitle: "Wyldgreens",
+        responseBool: true,
+        isError: ifError,
+      });
+      transporter.close();
+    });
 });
 
-// || Listener 
+// Listener
 
 const server = app.listen(0, () => {
-    console.log('Server running at port:', server.address().port);
+  console.log("Server running at port:", server.address().port);
 });
 
-// || END of document
+// END of document
